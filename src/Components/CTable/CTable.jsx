b8e274691e336b0cc4c6be9fa1bec5cb6ebe735d
ilyosbek9531 <iqboljonsheraliyev11@gmail.com>
2023-08-22 22:27:10 +0500

    react query table added

--- src/Components/CTable/CTable.jsx
+++ src/Components/CTable/CTable.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { MaterialReactTable } from "material-react-table";
 import { Box, IconButton, Tooltip } from "@mui/material";
 import { Delete } from "@mui/icons-material";
@@ -6,96 +6,129 @@ import { Delete } from "@mui/icons-material";
 const CTable = ({
   columns,
   data,
-  settings: {
-    enableColumnActions = true,
-    enableColumnFilters = true,
-    enablePagination = true,
-    enableSorting = true,
-    enableTopBottomToolbar = true,
-    muiTableBodyRowProps = true,
-    enableColumnResizing = true,
-    enableColumnOrdering = true,
-    enablePinning = true,
-    enableStickyHeader = true,
-    enableStickyFooter = true,
-    enableEditing = true,
-    enableRowNumbers = true,
-    editingMode = "modal",
-    enableRowActions = true,
-    enableClickToCopy = false,
-    enableColumnDragging = true,
-    enableColumnFilterModes = true,
-    enableExpanding = false,
+  settings = {
+    enableColumnActions: true,
+    enableColumnFilters: true,
+    enablePagination: true,
+    enableSorting: true,
+    enableTopBottomToolbar: true,
+    muiTableBodyRowProps: true,
+    enableColumnResizing: true,
+    enableColumnOrdering: true,
+    enablePinning: true,
+    enableStickyHeader: true,
+    enableStickyFooter: true,
+    enableEditing: true,
+    enableRowNumbers: true,
+    editingMode: "modal",
+    enableRowActions: true,
+    enableClickToCopy: false,
+    enableColumnDragging: true,
+    enableColumnFilterModes: true,
+    enableExpanding: false,
+    manualFiltering: false,
+    manualPagination: false,
+    manualSorting: false,
   },
-  border: {
-    borderFull = "1px solid black",
-    borderHeader = "1px solid black",
-    borderData = "1px solid black",
+  initialState,
+  border = {
+    borderFull: "1px solid black",
+    borderHeader: "1px solid black",
+    borderData: "1px solid black",
   },
   handleEditRow,
   handleDeleteRow,
-}) => (
-  <div id={!enableTopBottomToolbar && "mui-table"}>
-    <MaterialReactTable
-      // data
-      columns={columns}
-      data={data}
-      // settings
-      enableColumnActions={enableColumnActions}
-      enableColumnFilters={enableColumnFilters}
-      enablePagination={enablePagination}
-      enableSorting={enableSorting}
-      enableBottomToolbar={enableTopBottomToolbar}
-      enableTopToolbar={enableTopBottomToolbar}
-      enableColumnResizing={enableColumnResizing}
-      enableColumnOrdering={enableColumnOrdering}
-      enablePinning={enablePinning}
-      enableStickyHeader={enableStickyHeader}
-      enableStickyFooter={enableStickyFooter}
-      enableRowNumbers={enableRowNumbers}
-      muiTableBodyRowProps={{ hover: muiTableBodyRowProps }}
-      enableClickToCopy={enableClickToCopy}
-      enableColumnDragging={enableColumnDragging}
-      enableColumnFilterModes={enableColumnFilterModes}
-      enableExpanding={enableExpanding}
-      // edit
-      enableEditing={enableEditing}
-      editingMode={editingMode}
-      muiTableBodyCellEditTextFieldProps={({ cell }) => ({
-        //onBlur is more efficient, but could use onChange instead
-        onBlur: (event) => {
-          handleEditRow(cell, event.target.value);
-        },
-      })}
-      // delete
-      enableRowActions={enableRowActions}
-      renderRowActions={({ row, table }) => (
-        <Box sx={{ display: "flex", gap: "1rem" }}>
-          <Tooltip arrow placement="right" title="Delete">
-            <IconButton color="error" onClick={() => handleDeleteRow(row)}>
-              <Delete />
-            </IconButton>
-          </Tooltip>
-        </Box>
-      )}
-      // borders
-      muiTableProps={{
-        sx: {
-          border: borderFull,
-        },
-      }}
-      muiTableHeadCellProps={{
-        sx: {
-          border: borderHeader,
-        },
-      }}
-      muiTableBodyCellProps={{
-        sx: {
-          border: borderData,
-        },
-      }}
-    />
-  </div>
-);
+  muiToolbarAlertBannerProps,
+  onColumnFiltersChange,
+  onGlobalFilterChange,
+  onPaginationChange,
+  onSortingChange,
+  renderTopToolbarCustomActions,
+  rowCount,
+  state,
+}) => {
+  return (
+    <div id={!settings.enableTopBottomToolbar && "mui-table"}>
+      <MaterialReactTable
+        // data
+        columns={columns}
+        data={data}
+        // settings
+        enableColumnActions={settings.enableColumnActions}
+        enableColumnFilters={settings.enableColumnFilters}
+        enablePagination={settings.enablePagination}
+        enableSorting={settings.enableSorting}
+        enableBottomToolbar={settings.enableTopBottomToolbar}
+        enableTopToolbar={settings.enableTopBottomToolbar}
+        enableColumnResizing={settings.enableColumnResizing}
+        enableColumnOrdering={settings.enableColumnOrdering}
+        enablePinning={settings.enablePinning}
+        enableStickyHeader={settings.enableStickyHeader}
+        enableStickyFooter={settings.enableStickyFooter}
+        enableRowNumbers={settings.enableRowNumbers}
+        muiTableBodyRowProps={{ hover: settings.muiTableBodyRowProps }}
+        enableClickToCopy={settings.enableClickToCopy}
+        enableColumnDragging={settings.enableColumnDragging}
+        enableColumnFilterModes={settings.enableColumnFilterModes}
+        enableExpanding={settings.enableExpanding}
+        // edit
+        enableEditing={settings.enableEditing}
+        editingMode={settings.editingMode}
+        muiTableBodyCellEditTextFieldProps={({ cell }) => ({
+          //onBlur is more efficient, but could use onChange instead
+          onBlur: (event) => {
+            handleEditRow(cell, event.target.value);
+          },
+        })}
+        // delete
+        enableRowActions={settings.enableRowActions}
+        renderRowActions={({ row, table }) => (
+          <Box sx={{ display: "flex", gap: "1rem" }}>
+            <Tooltip arrow placement="right" title="Delete">
+              <IconButton color="error" onClick={() => handleDeleteRow(row)}>
+                <Delete />
+              </IconButton>
+            </Tooltip>
+          </Box>
+        )}
+        // borders
+        muiTableProps={{
+          sx: {
+            border: border.borderFull,
+          },
+        }}
+        muiTableHeadCellProps={{
+          sx: {
+            border: border.borderHeader,
+          },
+        }}
+        muiTableBodyCellProps={{
+          sx: {
+            border: border.borderData,
+          },
+        }}
+        // initial state
+        initialState={initialState}
+        //manual
+        manualFiltering={settings.manualFiltering}
+        manualPagination={settings.manualPagination}
+        manualSorting={settings.manualSorting}
+        //error
+        muiToolbarAlertBannerProps={muiToolbarAlertBannerProps}
+        // on change
+        onColumnFiltersChange={onColumnFiltersChange}
+        onGlobalFilterChange={onGlobalFilterChange}
+        onPaginationChange={onPaginationChange}
+        onSortingChange={onSortingChange}
+        // custom action
+        renderTopToolbarCustomActions={renderTopToolbarCustomActions}
+        //rowCount
+        rowCount={rowCount}
+        //state
+        state={state}
+      />
+    </div>
+  );
+};
 
 export default CTable;
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,21 +1,21 @@
 import CTable from "Components/CTable/CTable";
-import React, { useMemo, useState } from "react";
+import React, { useState } from "react";
 import { Outlet, useParams } from "react-router-dom";
-import { Box, Switch } from "@mui/material";
-import { data } from "./data";
+import { IconButton, Tooltip } from "@mui/material";
 import { UseGetProducts } from "services/fake.service";
+import RefreshIcon from "@mui/icons-material/Refresh";
 
 const Main = () => {
   const { id } = useParams();
-  // react-query table
   const [columnFilters, setColumnFilters] = useState([]);
   const [globalFilter, setGlobalFilter] = useState("");
   const [sorting, setSorting] = useState([]);
   const [pagination, setPagination] = useState({
-    pageIndex: 1,
+    pageIndex: 0,
     pageSize: 10,
   });
 
+  console.log("pagination", pagination);
   const { data, isError, isFetching, isLoading, refetch } = UseGetProducts({
     queryParams: {
       offset: pagination.pageIndex * pagination.pageSize,
@@ -53,149 +53,9 @@ const Main = () => {
     },
   ];
 
-  //  simple table
-
-  // const [tableData, setTableData] = useState(() => data);
-
-  // const columns = useMemo(
-  //   () => [
-  //     {
-  //       accessorFn: (row) => `${row.name.firstName} ${row.name.lastName}`,
-  //       header: "Full Name",
-  //       Cell: ({ renderedCellValue, row }) => {
-  //         return (
-  //           <Box
-  //             sx={{
-  //               display: "flex",
-  //               alignItems: "center",
-  //               gap: "1rem",
-  //             }}
-  //           >
-  //             <img
-  //               alt="avatar"
-  //               height={30}
-  //               src={row.original.avatar}
-  //               loading="lazy"
-  //               style={{ borderRadius: "50%" }}
-  //             />
-  //             <span>{renderedCellValue}</span>
-  //           </Box>
-  //         );
-  //       },
-  //     },
-  //     {
-  //       accessorKey: "name.firstName",
-  //       header: "First Name",
-  //       size: 150,
-  //     },
-  //     {
-  //       accessorKey: "name.lastName",
-  //       header: "Last Name",
-  //       size: 150,
-  //     },
-  //     {
-  //       accessorKey: "address", //normal accessorKey
-  //       header: "Address",
-  //       size: 300,
-  //     },
-  //     {
-  //       accessorKey: "city",
-  //       header: "City",
-  //       size: 150,
-  //     },
-  //     {
-  //       accessorKey: "state",
-  //       header: "State",
-  //       size: 150,
-  //     },
-  //     {
-  //       accessorKey: "status",
-  //       header: "Status",
-  //       size: 150,
-  //       Cell: ({ renderedCellValue, row }) => {
-  //         return (
-  //           <Box
-  //             sx={{
-  //               display: "flex",
-  //               alignItems: "center",
-  //               gap: "1rem",
-  //             }}
-  //           >
-  //             <Switch
-  //               checked={row.status}
-  //               inputProps={{ "aria-label": "controlled" }}
-  //             />
-  //             {/* <span>{renderedCellValue}</span> */}
-  //           </Box>
-  //         );
-  //       },
-  //     },
-  //   ],
-  //   []
-  // );
-
   return (
     <div>
       {!id && (
-        // simgple table
-        // <CTable
-        //   data={tableData}
-        //   columns={columns}
-        //   settings={{
-        //     enableColumnActions: true,
-        //     enableColumnFilters: true,
-        //     enablePagination: true,
-        //     enableSorting: true,
-        //     enableTopBottomToolbar: true,
-        //     muiTableBodyRowProps: true,
-        //     enableColumnResizing: true,
-        //     enableColumnOrdering: true,
-        //     enablePinning: true,
-        //     enableStickyHeader: true,
-        //     enableStickyFooter: true,
-        //     enableEditing: (row) => {
-        //       return row.original.status;
-        //     },
-        //     enableRowNumbers: true,
-        //     editingMode: "cell",
-        //     enableRowActions: true,
-        //     enableClickToCopy: false,
-        //     enableColumnDragging: true,
-        //     enableColumnFilterModes: true,
-        //     enableExpanding: false,
-        //   }}
-        //   border={{
-        //     borderFull: "1px solid #e1e5e8",
-        //     borderHeader: "1px solid #e1e5e8",
-        //     borderData: "1px solid #e1e5e8",
-        //   }}
-        //   handleEditRow={(cell, value) => {
-        //     //if using flat data and simple accessorKeys/ids, you can just do a simple assignment here
-        //     let keys = cell.column.id.split(".");
-        //     let newObj = {};
-        //     keys.forEach((element, i) => {
-        //       if (i === 0) {
-        //         newObj = tableData[cell.row.index][element];
-        //       } else {
-        //         if (typeof newObj[element] === "object")
-        //           newObj = newObj[element];
-        //       }
-        //     });
-
-        //     if (keys.length > 1) {
-        //       newObj[keys[keys.length - 1]] = value;
-        //     } else tableData[cell.row.index][cell.column.id] = value;
-
-        //     //send/receive api updates here
-        //     setTableData([...tableData]); //re-render with new data
-        //   }}
-        //   handleDeleteRow={(row) => {
-        //     const newTableData = tableData.filter(
-        //       (_, index) => index !== row.index
-        //     );
-        //     setTableData(newTableData);
-        //   }}
-        // />
         <CTable
           data={data?.data ?? []}
           columns={columns ?? columnsLoading}
@@ -215,12 +75,12 @@ const Main = () => {
               return row.original.status;
             },
             enableRowNumbers: true,
-            editingMode: "cell",
+            editingMode: "modal",
             enableRowActions: true,
-            enableClickToCopy: false,
+            enableClickToCopy: true,
             enableColumnDragging: true,
             enableColumnFilterModes: true,
-            enableExpanding: false,
+            enableExpanding: true,
           }}
           border={{
             borderFull: "1px solid #e1e5e8",
@@ -250,6 +110,43 @@ const Main = () => {
             //   (_, index) => index !== row.index
             // );
             // setTableData(newTableData);
+          }}
+          // set initial state
+          initialState={{ showColumnFilters: false }}
+          // manual
+          manualFiltering
+          manualPagination
+          manualSorting
+          //error message
+          muiToolbarAlertBannerProps={
+            isError
+              ? {
+                  color: "error",
+                  children: "Error loading data",
+                }
+              : undefined
+          }
+          // on Change
+          onColumnFiltersChange={setColumnFilters}
+          onGlobalFilterChange={setGlobalFilter}
+          onSortingChange={setSorting}
+          onPaginationChange={setPagination}
+          renderTopToolbarCustomActions={() => (
+            <Tooltip arrow title="Refresh Data">
+              <IconButton onClick={() => refetch()}>
+                <RefreshIcon />
+              </IconButton>
+            </Tooltip>
+          )}
+          rowCount={data?.count ?? 0}
+          state={{
+            columnFilters,
+            globalFilter,
+            isLoading,
+            pagination,
+            showAlertBanner: isError,
+            showProgressBars: isFetching,
+            sorting,
           }}
         />
       )}
