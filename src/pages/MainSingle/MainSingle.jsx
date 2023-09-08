4799ad366c29c9ab833693aecee1d5a6991dc06e
ilyosbek9531 <iqboljonsheraliyev11@gmail.com>
2023-09-08 22:51:34 +0500

    fixed

--- src/pages/MainSingle/MainSingle.jsx
+++ src/pages/MainSingle/MainSingle.jsx
@@ -8,6 +8,7 @@ const MainSingle = () => {
   const expandedSinglePage = useSelector(
     (state) => state.sidebar.expandSinglePage
   );
+  const expanded = useSelector((state) => state.sidebar.expand);
   return (
     <div className={styles.mainSingle}>
       <div className={styles.mainSingle__base}>
@@ -17,7 +18,7 @@ const MainSingle = () => {
         className="main_single"
         style={{
           marginLeft: `${!expandedSinglePage ? "0px" : "300px"}`,
-          width: expandedSinglePage ? "75%" : "100%",
+          width: expandedSinglePage ? (expanded ? "75%" : "78%") : "100%",
         }}
       >
         <MainSingleRelations />
